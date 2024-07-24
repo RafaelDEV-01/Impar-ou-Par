da5ab14185031c3eb76abb69ffc43235010f7d58
José Rafael <166348491+RafaelDEV-01@users.noreply.github.com>
2024-07-23 21:45:56 -0300

    Add files via upload

---
+++ balao.java
@@ -0,0 +1,29 @@
+package basico;
+import java.util.Scanner;
+public class balao {
+	public static void main(String[] args) {
+		Scanner i = new Scanner(System.in);
+		int n1;
+		System.out.print("Digite um Número:");
+		n1 = i.nextInt();
+		if(n1%2==0)
+		{System.out.printf("O Número %d e Par!",n1);}
+		else {System.out.printf("O Número %d e Impar!",n1);}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
