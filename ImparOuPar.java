8d941c7caa03b4548ba3f815955c3d9c1caa357f
José Rafael <166348491+RafaelDEV-01@users.noreply.github.com>
2024-07-23 21:46:39 -0300

    Update and rename balao.java to ImparOuPar.java

--- ImparOuPar.java
+++ ImparOuPar.java
@@ -1,6 +1,6 @@
 package basico;
 import java.util.Scanner;
-public class balao {
+public class ImparOuPar {
 	public static void main(String[] args) {
 		Scanner i = new Scanner(System.in);
 		int n1;
